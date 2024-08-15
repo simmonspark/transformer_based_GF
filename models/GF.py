72cff163f844033bdf98d9f351c3c4293e5d2c97
sien <sptw88@gmail.com>
2024-08-15 22:14:11 +0900

    TEST 작성 완료

--- models/GF.py
+++ models/GF.py
@@ -51,32 +51,38 @@ class GF(nn.Module):
 def count_parameters(model):
     return sum(p.numel() for p in model.parameters())
 
+
 print('-------------------------------')
-print('------model test activate------')
-print('-------------------------------')
+print('--- 내 친구  compile TEST :) ----')
+print('-------------------------------\n')
 
 config = GFConfig()
 model = GF(config)
-print(f'[PASSED] at. configuration test')
+print(f'[PASSED] at. configuration test\n')
 source = torch.randint(0, config.vocab_size, (8, 1024)).long()
 target = torch.randint(0, config.vocab_size, (8, 1024)).long()
-print(f'[PASSED] at. generate dummy IO')
+print(f'[PASSED] at. generate dummy IO\n')
 out = model(source, target)
-print(f'[PASSED] at. test model out. shape as below')
+print(f'[PASSED] at. test model out. shape as below\n')
 print(out.shape)
+print('\n')
 criterion = nn.CrossEntropyLoss()
 
 out_reshaped = out.view(-1, config.vocab_size)  # (batch_size * target_len, vocab_size)
 
 target_reshaped = target.view(-1)  # (batch_size * target_len)
-print(f'[PASSED] at. CAL nan loss as randn IO data')
+print(f'[PASSED] at. CAL nan loss as randn IO data\n')
 loss = criterion(out_reshaped, target_reshaped)
 
 print("Loss:", loss.item())
+print('\n')
 n = count_parameters(model)
-print(f'[PASSED] at. countion model params')
-print(f'params : {n:.4f}')
-
-
-
+print(f'[PASSED] at. countion model params\n')
+print(f'params : {n / 1_000_000:.4f}M')
+print('\n짝짝짝~ 모든 필수 assertion 테스트를 통과했어요~\n')
+print('\n모델 고속 컴파일...\n')
+model = torch.compile(model)
+print('\nDONE\n')
+print('data preprocessing 프로세스가 작동합니다.')
+print('...시간이 좀 걸려요...\n')
 
